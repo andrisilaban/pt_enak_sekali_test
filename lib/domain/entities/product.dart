42622307daea3c278765f945d6a057af139fa4dc
andrisilaban <andriantosilaban@gmail.com>
2025-03-05 16:32:01 +0700

    Add Product Model

---
+++ lib/domain/entities/product.dart
@@ -0,0 +1,21 @@
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+  final String category;
+  final String thumbnail;
+  final double rating;
+  final String shippingInformation;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.category,
+    required this.thumbnail,
+    required this.rating,
+    required this.shippingInformation,
+  });
+}
